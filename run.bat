89aeb0e9093ac064529d9fceb195cd47753394e2
tonytan-tanapon <tonytan.tanapon@gmail.com>
2025-09-16 22:05:06 -0700

    run edit

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
-@echo off
+@echo on
+cd /d C:\Users\Tanapon\Documents\GitHub\cnc
 call venv\Scripts\activate.bat
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+pause
