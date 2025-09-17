f162f8f057a2afc15478a5a8c3f2c4746d22cbd2
tonytan-tanapon <tonytan.tanapon@gmail.com>
2025-09-16 22:25:04 -0700

    run

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d C:\Users\TPSERVER\cnc_project
 call venv\Scripts\activate.bat
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn main:app --host 0.0.0.0 --port 8000
 pause
