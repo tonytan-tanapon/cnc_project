db1b4669a9368acfc9de723f067b0960bec94b99
Tony Tanapon <tonytan.tanapon@gmail.com>
2025-09-16 22:17:39 -0700

    update

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d C:\Users\Tanapon\Documents\GitHub\cnc
+cd /d C:\Users\TPSERVER\dev\cnc_project
 call venv\Scripts\activate.bat
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 pause
