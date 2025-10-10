ef56bdb5fa297fe0d1bf5e7d391a309ea6f2285a
Tony Tanapon Tantisripreecha <tonytan.tanapon@gmail.com>
2025-10-10 10:52:13 -0700

    sub

--- deploy_sub.bat
+++ deploy_sub.bat
@@ -5,9 +5,9 @@ echo === Pulling latest code from Git ===
 git pull
 
 echo === Restarting server ===
-call stop_server.bat
+call stop_server_sub.bat
 
 :: รอจนกว่าพอร์ต 8000 จะว่าง (กัน error 10048)
 powershell -NoProfile -Command "while(Get-NetTCPConnection -State Listen -LocalPort 8000 -ErrorAction SilentlyContinue){ Start-Sleep -Milliseconds 200 }"
 
-call start_server.bat
+call start_server_sub.bat
