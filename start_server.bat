02f94c3366e2dd519d54edc1940f30816ebd8200
Tony Tanapon <tonytan.tanapon@gmail.com>
2025-10-10 12:43:02 -0700

    test

--- start_server.bat
+++ start_server.bat
@@ -3,7 +3,7 @@ cd /d "C:\Users\TPSERVER\cnc_project"
 call venv\Scripts\activate.bat
 
 echo === Starting server (port 8000) ===
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 9000
 
 echo === Server stopped ===
 pause
